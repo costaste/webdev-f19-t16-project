957518df9016c791111e23f0f5b8d34a1a56647f
JillianBurns <jillian.jb.burns@gmail.com>
2019-12-10 09:27:17 -0500

    simplified home image component, removed resizing that distorted image

--- src/Home/Components/HomeImage.js
+++ src/Home/Components/HomeImage.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import '../Home.css';
 
-const HomeImage = () => {
-    const imageUrls = [
-        'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2013/03/mixer.jpg',
-        'https://images.freecreatives.com/wp-content/uploads/2016/04/HD-DJ-Headphones-Wallpaper.jpg',
-        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapersite.com%2Fimages%2Fwallpapers%2Fheadphones-2560x1440-yellow-background-hd-5k-10170.jpg&f=1&nofb=1',
-        'https://hdqwalls.com/wallpapers/microphone-metal.jpg'
-    ];
-
-    const randInt = (max) => {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
-    var background = {backgroundSize : 'cover'};
-
-    const src = imageUrls[randInt(imageUrls.length)];
-    return (
-        <div>
-            <img
-                src={src}
-                style={background} responsive
-                className='t16-home-image'
-                alt={src}
-            />
-            <h1><span>Welcome</span></h1>
-        </div>
-    )
-}
+const HomeImage = ({src}) =>
+    <img
+        src={src}
+        alt={src}
+        className='t16-home-image'
+    />;
 
 export default HomeImage;
--- src/Home/Containers/Content.js
+++ src/Home/Containers/Content.js
@@ -3,10 +3,28 @@ import HomeImage from '../Components/HomeImage';
 import PolicyContainer from '../../Privacy/Containers/PolicyContainer';
 
 export default class Content extends React.Component {
+    src;
+
+    constructor(props) {
+        super(props);
+
+        const imageUrls = [
+            'https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2013/03/mixer.jpg',
+            'https://images.freecreatives.com/wp-content/uploads/2016/04/HD-DJ-Headphones-Wallpaper.jpg',
+            'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapersite.com%2Fimages%2Fwallpapers%2Fheadphones-2560x1440-yellow-background-hd-5k-10170.jpg&f=1&nofb=1',
+            'https://hdqwalls.com/wallpapers/microphone-metal.jpg'
+        ];
+
+        const randInt = max => Math.floor(Math.random() * Math.floor(max));
+
+        this.src = imageUrls[randInt(imageUrls.length)];
+    }
+
     render() {
         return (
             <div>
-                <HomeImage />
+                <HomeImage src={this.src}/>
+                <h1><span>Welcome</span></h1>
                 <PolicyContainer />
             </div>
         );
