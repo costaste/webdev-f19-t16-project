3ebb550ef715ef4052e525107c55786f06e138a5
Stephen Costa <st3ph3n24@gmail.com>
2019-12-10 10:32:57 -0500

    space out buttons

--- src/User/Components/ProfileComponent.js
+++ src/User/Components/ProfileComponent.js
@@ -48,13 +48,13 @@ const ProfileComponent = ({user, username, currentPassword, password, verifyPass
             />
             <button
                 onClick={validateUser}
-                className="btn btn-primary"
+                className="btn btn-primary t16-profile-btn"
             >
                     Update
             </button>
             <button
                 onClick={logout}
-                className="btn btn-outline-danger"
+                className="btn btn-outline-danger t16-profile-btn"
             >
                     Logout
             </button>
